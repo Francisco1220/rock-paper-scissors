6fa310722b48c4df1a16ccd17097f1df99b0cc08
Francisco <franciscocolombocouste@gmail.com>
2024-05-14 21:33:52 +0200

    Initialised variables that will store points for each round

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-// Get's random choice from computer
+
 
 const options = ["rock", "paper", "scissors"]
 
+// Get's random choice from computer
 function getComputerChoice () {
   const compChoice = options[Math.floor(Math.random() * options.length)];
   return compChoice;
 }
 
-function getHumanChoice () {
-    const humanChoice = prompt("Rock, Paper, or Scissors?", "rock");
-    return humanChoice;
+// Get's user input. RETURN keyword ensures I can use value outside the function (function scope)
+function getUserChoice () {
+    const userChoice = prompt("Rock, Paper, or Scissors?", "rock");
+    return userChoice;
 }
+
+// Initialised variables that will store points
+let computerScore = 0;
+let userScore = 0;
+
+console.log(computerScore);
+console.log(userScore);
