cf497362c814be915a58774b6f426d5c9be63697
Francisco <franciscocolombocouste@gmail.com>
2024-06-08 17:49:24 +0200

    Displays winner of each round

--- main.js
+++ main.js
@@ -21,27 +21,27 @@ let userScore = 0;
 // Play's one round of rps. It checks the winner, increments userScore or computerScore accordingly, and logs a message declaring the winner
 function playRound (userInput, computerChoice) {
     if (userInput === "rock" & computerChoice === "scissors") {
-        console.log("You Win this Round! Rock beats Scissors.");
+        displayRoundResult.textContent = "You Win this Round! Rock beats Scissors.";
         userScore++;
     } else if (userInput === "rock" && computerChoice === "paper") {
-        console.log("You Lose this Round! Paper beats rock.");
+        displayRoundResult.textContent = "You Lose this Round! Paper beats rock.";
         computerScore++;
     } else if (userInput === "paper" && computerChoice === "rock") {
-        console.log("You Win this Round! Paper beats Rock.");
+        displayRoundResult.textContent = "You Win this Round! Paper beats Rock.";
         userScore++;
     } else if (userInput === "paper" && computerChoice === "scissors") {
-        console.log ("You Lose this Round! Scissors beats Paper.");
+        displayRoundResult.textContent = "You Lose this Round! Scissors beats Paper.";
         computerScore++;
     } else if (userInput === "scissors" && computerChoice === "paper") {
-        console.log("You Win this Round! Scissors beats Paper.");
+        displayRoundResult.textContent = "You Win this Round! Scissors beats Paper.";
         userScore++;
     } else if (userInput === "scissors" && computerChoice === "rock") {
-        console.log("You Lose this Round! Rock beats Scissors.");
+        displayRoundResult.textContent = "You Lose this Round! Rock beats Scissors.";
         computerScore++;
     } else if (userInput === "rock" && computerChoice === "rock" || userInput === "paper" && computerChoice === "paper" || userInput === "scissors" && computerChoice === "scissors") {
-        console.log("Tie!");
+        displayRoundResult.textContent = "Tie!";
     } else {
-        console.log("I think you made a mistake there...");
+        displayRoundResult.textContent = "I think you made a mistake there...";
     }
 }
 
@@ -49,10 +49,11 @@ const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
 const btn3 = document.querySelector("#btn3");
 
-const displayResult = document.querySelector("#div2");
-displayResult.style.marginBottom="10px";
+const displayScore = document.querySelector("#div2");
+displayScore.style.marginBottom="10px";
 const displayWinner = document.querySelector("#div3");
-
+const displayRoundResult = document.querySelector("#div1");
+displayRoundResult.style.marginBottom="10px";
 
 const btn = document.querySelectorAll(".btn");
 btn[0].style.marginRight="10px";
@@ -62,22 +63,21 @@ btn[0].style.marginBottom="10px";
 btn[1].style.marginBottom="10px";
 btn[2].style.marginBottom="10px";
 
-
 btn1.addEventListener("click", () => {
     playRound(userInput = "rock", getComputerChoice());
-    displayResult.textContent = `User Score: ${userScore} Computer Score: ${computerScore}`;
+    displayScore.textContent = `User Score: ${userScore} Computer Score: ${computerScore}`;
     checksWinner();
 });
 
 btn2.addEventListener("click", () => {
     playRound(userInput = "paper", getComputerChoice());
-    displayResult.textContent = `User Score: ${userScore} Computer Score: ${computerScore}`;
+    displayScore.textContent = `User Score: ${userScore} Computer Score: ${computerScore}`;
     checksWinner();
 });
 
 btn3.addEventListener("click", () => {
     playRound(userInput = "scissors", getComputerChoice())
-    displayResult.textContent = `User Score: ${userScore} Computer Score: ${computerScore}`;
+    displayScore.textContent = `User Score: ${userScore} Computer Score: ${computerScore}`;
     checksWinner();
 });
 
